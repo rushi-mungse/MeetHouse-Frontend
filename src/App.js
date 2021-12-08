8d7a450f640c2dc105001a92fd92d07292a9596c
Rushikesh Mungse <mungse.rushi@gmail.com>
2021-12-08 10:11:59 +0530

    creating protected routes in react-router v6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom'
 import Navigation from './components/Navigation/Navigation';
 import Home from './pages/Home/Home';
 import Authenticate from './pages/Authenticate/Authenticate'
 import Activate from './pages/Activate/Activate';
+import Rooms from './pages/Rooms/Rooms';
+
+const isAuth = true;
+const user = {
+  activated: true
+}
 
 function App() {
   return (
@@ -11,13 +17,33 @@ function App() {
       <Router>
         <Navigation />
         <Routes>
-          <Route exact element={<Home />} path='/' />
-          <Route element={<Authenticate />} path='/authenticate' />
-          <Route element={<Activate />} path='/activate' />
+
+          <Route element={<GuestRoute />}>
+            <Route index element={<Home />} path='/' />
+          </Route>
+          <Route element={<GuestRoute />}>
+            <Route element={<Authenticate />} path='/authenticate' />
+          </Route>
+          <Route element={<SemiProtectedRoute />} >
+            <Route element={<Activate />} path='/activate' />
+          </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/rooms" element={<Rooms />} />
+          </Route>
         </Routes>
+
       </Router>
     </>
   );
 }
 
+const GuestRoute = () => {
+  return isAuth ? <Navigate to="/rooms" /> : <Outlet />
+}
+const SemiProtectedRoute = () => {
+  return !isAuth ? <Navigate to="/" /> : isAuth && !user.activated ? <Outlet /> : <Navigate to="/rooms" />
+}
+const ProtectedRoute = () => {
+  return !isAuth ? <Navigate to="/" /> : isAuth && !user.activated ? <Navigate to="/activate" /> : <Outlet />
+}
 export default App;
