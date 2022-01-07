7828001e2f3e6511cf2f786a2abe29fb82c1f855
Rushikesh Mungse <mungse.rushi@gmail.com>
2022-01-07 09:42:17 +0530

    user itself connected with audio

--- src/hooks/useWebRTC.js
+++ src/hooks/useWebRTC.js
@@ -1,17 +1,40 @@
 import { useStateWithWebRTC } from './useStateWithWebRTC'
- const users = [
-        {
-            id: 1,
-            name: "Rushikesh M"
-        },
-        {
-            id: 2,
-            name: "Vaibhav V"
-        }
-    ]
-export const useWebRTC = ({roomId,user}) => {
-   
-    const [clients, setClients] = useStateWithWebRTC(users)
+import { useEffect, useRef, useCallback, } from 'react'
 
-    return { clients }
+export const useWebRTC = ( roomId, user ) => {
+    const [clients, setClients] = useStateWithWebRTC([])
+    const audioElements = useRef({});
+    const connections = useRef({});
+    const localMediaStream = useRef(null);
+
+    const addNewClient = useCallback(
+        (newClient, cb) => {
+            const lookingFor = clients.find((client) => client._id === newClient._id);
+            if (lookingFor === undefined) {
+                setClients((existingClients) => [...existingClients, newClient], cb)
+            }
+        },
+        [clients, setClients],
+    )
+    useEffect(() => {
+        const startCapture = async () => {
+            localMediaStream.current = await navigator.mediaDevices.getUserMedia({
+                audio: true
+            });
+        }
+        startCapture().then(() => {
+            addNewClient(user, () => {
+                const localElement = audioElements.current[user._id];
+                if (localElement) {
+                    localElement.volume = 0;
+                    localElement.srcObject = localMediaStream.current;
+                }
+            })
+        })
+    }, [])
+
+    const provideRef = (instance, userId) => {
+        audioElements.current[userId] = instance;
+    }
+    return { clients, provideRef }
 }
--- src/pages/RoomForClient/RoomForClient.jsx
+++ src/pages/RoomForClient/RoomForClient.jsx
@@ -6,14 +6,18 @@ import { useSelector } from "react-redux";
 const RoomForClient = () => {
   const { id: roomId } = useParams();
   const user = useSelector((state) => state.auth.user);
-  const { clients } = useWebRTC(roomId, user);
+  const { clients, provideRef } = useWebRTC(roomId, user);
   return (
     <div>
       <h1>All connected clients</h1>
       {clients.map((client) => {
         return (
-          <div key={client.id}>
-            <audio src="" controls autoPlay></audio>
+          <div key={client._id}>
+            <audio
+              ref={(instance) => provideRef(instance, client._id)}
+              controls
+              autoPlay
+            ></audio>
             <h4>{client.name}</h4>
           </div>
         );
