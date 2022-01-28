4f36707bab02c0e82f31218589061db7f3f158fe
Rushikesh Mungse <mungse.rushi@gmail.com>
2022-01-29 01:43:20 +0530

    socket.io and webRTC connection

--- src/hooks/useWebRTC.js
+++ src/hooks/useWebRTC.js
@@ -1,12 +1,18 @@
 import { useStateWithWebRTC } from './useStateWithWebRTC'
 import { useEffect, useRef, useCallback, } from 'react'
+import { socketInit } from '../socket'
+import { ACTIONS } from '../actions'
+import freeice from 'freeice'
 
-export const useWebRTC = ( roomId, user ) => {
+export const useWebRTC = (roomId, user) => {
     const [clients, setClients] = useStateWithWebRTC([])
     const audioElements = useRef({});
     const connections = useRef({});
     const localMediaStream = useRef(null);
-
+    const socket = useRef(null)
+    useEffect(() => {
+        socket.current = socketInit();
+    }, [])
     const addNewClient = useCallback(
         (newClient, cb) => {
             const lookingFor = clients.find((client) => client._id === newClient._id);
@@ -29,10 +35,115 @@ export const useWebRTC = ( roomId, user ) => {
                     localElement.volume = 0;
                     localElement.srcObject = localMediaStream.current;
                 }
+                //use WebRTC
+                socket.current.emit(ACTIONS.JOIN, { roomId, user });
             })
         })
+        return ()=>{
+            localMediaStream.current.getTracks().forEach(track=>track.stop())
+            socket.current.emit(ACTIONS.LEAVE,{roomId})
+        }
+    }, [])
+    useEffect(() => {
+        const handleNewPeer = async ({ peerId, createOffer, user: remoteUser }) => {
+            if (peerId in connections.current) {
+                return console.warn(`You are already connected with ${user.name}`)
+            }
+            connections.current[peerId] = new RTCPeerConnection({
+                iceServers: freeice()
+            })
+            connections.current[peerId].onicecandidat = (event) => {
+                socket.current.emit(ACTIONS.RELAY_ICE, {
+                    peerId,
+                    icecandidate: event.candidate,
+                })
+            }
+            connections.current[peerId].ontrack = ({
+                streams: [remoteStream]
+            }) => {
+                addNewClient(remoteUser, () => {
+                    if (audioElements.current[remoteUser._id]) {
+                        audioElements.current[remoteUser._id].srcObject = remoteStream
+                    } else {
+                        let settled = false;
+                        const interval = setInterval(() => {
+                            if (audioElements.current[remoteUser._id]) {
+                                audioElements.current[remoteUser._id].srcObject = remoteStream
+                                settled = true
+                            }
+                            if (settled) {
+                                clearInterval(interval);
+                            }
+                        }, 1000)
+                    }
+                })
+            }
+            localMediaStream.current.getTracks().forEach(track => {
+                connections.current[peerId].addTrack(track, localMediaStream.current);
+
+            });
+            if (createOffer) {
+                const offer = await connections.current[peerId].createOffer();
+                await connections.current[peerId].setLocalDescription(offer);
+                socket.current.emit(ACTIONS.RELAY_SDP, {
+                    peerId,
+                    sessionDiscription: offer
+                })
+            }
+        }
+        socket.current.on(ACTIONS.ADD_PEER, handleNewPeer)
+        return () => {
+            socket.current.off(ACTIONS.ADD_PEER)
+        }
+
+    }, [])
+    useEffect(() => {
+        socket.current.on(ACTIONS.ICE_CANDIDATE, ({ peerId, icecandidate }) => {
+            if (icecandidate) {
+                connections.current[peerId].addIceCandidate(icecandidate);
+            }
+        })
+        return () => {
+            socket.current.off(ACTIONS.ICE_CANDIDATE)
+        }
+    }, [])
+    useEffect(() => {
+        const handleRemoteSdp = async ({ peerId, sessionDiscription: remoteSessionDescription }) => {
+
+            connections.current[peerId].setRemoteDescription(
+                new RTCSessionDescription(remoteSessionDescription)
+            )
+            if (remoteSessionDescription === "offer") {
+                const connection = connections.current[peerId];
+                const answer = await connection.createAnswer()
+                connection.setLocalDescription(answer);
+                socket.current.emit(ACTIONS.RELAY_SDP, {
+                    peerId,
+                    sessionDescription: answer
+                })
+            }
+        }
+        socket.current.on(ACTIONS.SESSION_DESCRIPTION, handleRemoteSdp)
+        return () => {
+            socket.current.off(ACTIONS.SESSION_DESCRIPTION)
+        }
     }, [])
 
+    useEffect(() => {
+        const handleRemovePeer = async ({ peerId }) => {
+            if (connections.current[peerId]) {
+                connections.current[peerId].close();
+            }
+            delete connections.current[peerId];
+            delete audioElements.current[peerId];
+            setClients(list => list.filter(client => client._id !== user._id))
+        }
+        socket.current.on(ACTIONS.REMOVE_PEER, handleRemovePeer)
+
+        return () => {
+            socket.current.off(ACTIONS.REMOVE_PEER)
+        }
+    }, [])
     const provideRef = (instance, userId) => {
         audioElements.current[userId] = instance;
     }
--- src/pages/RoomForClient/RoomForClient.jsx
+++ src/pages/RoomForClient/RoomForClient.jsx
@@ -1,27 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useWebRTC } from "../../hooks/useWebRTC";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { getRoomInfo } from "../../http";
+import styles from "./RoomForClient.module.css";
+import { useNavigate } from "react-router-dom";
 
 const RoomForClient = () => {
   const { id: roomId } = useParams();
   const user = useSelector((state) => state.auth.user);
   const { clients, provideRef } = useWebRTC(roomId, user);
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await getRoomInfo({ roomId });
+        setData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, [roomId]);
+  const navigate=useNavigate()
+  const backPage=(e)=>{
+    e.preventDefault()
+    navigate("/rooms") 
+  }
   return (
-    <div>
-      <h1>All connected clients</h1>
-      {clients.map((client) => {
-        return (
-          <div key={client._id}>
-            <audio
-              ref={(instance) => provideRef(instance, client._id)}
-              controls
-              autoPlay
-            ></audio>
-            <h4>{client.name}</h4>
-          </div>
-        );
-      })}
+    <div className={styles.roomForClient}>
+      <div className={styles.button_wrapper}>
+        <button className={styles.backButton} onClick={backPage}>
+          <img src="/images/arrow-left.png" alt="back_button" />
+          <h1>All voice Rooms</h1>
+        </button>
+        <div className={styles.reatingForUser}>
+          <img src="/images/win.png" alt="win" />
+          <img
+            src="/images/red-heart.png"
+            alt="heart"
+            className={styles.heart}
+          />
+        </div>
+      </div>
+      {data && (
+        <div className={styles.topic}>
+          <h1>
+            <img src="/images/fire.png" alt="fire" /> {data.room.topic}
+          </h1>
+          <button>Leave Room</button>
+        </div>
+      )}
+      <div className={styles.clientsWrap}>
+        {clients.map((client) => {
+          return (
+            <div key={client._id} className={styles.client}>
+              <audio
+                ref={(instance) => provideRef(instance, client._id)}
+                controls
+                autoPlay
+              ></audio>
+              <img src={client.avatar} alt="avatar" />
+              <h4>{client.name}</h4>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
